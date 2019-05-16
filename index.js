17dbabac1feb52e5c0e6d61ea46120848e2ffb30
andres caffarena <andrescaffarena@hotmail.com>
2019-05-15 21:30:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        entradas: [
+            { campo: 'Marca' },
+            { campo: 'Modelo' },
+            { campo: 'Serial' },
+            {campo: 'extras'}
+          ]
+    }
+});
